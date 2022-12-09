eb570a31b7f307ae6e433425fa93d75961491cd3
Anton Zaynutdinov <zaunytdinov@mail.ru>
2022-12-10 01:57:24 +0300

    initial commit

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.config.service.CarService;
-import web.config.service.CarServiceImp;
+import web.service.CarService;
+import web.service.CarServiceImp;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/web/service/CarServiceImp.java
+++ src/main/java/web/service/CarServiceImp.java
@@ -1,4 +1,4 @@
-package web.config.service;
+package web.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;

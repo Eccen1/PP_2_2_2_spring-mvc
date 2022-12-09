c023adcbf0f86a02c857d5673e946d840d5025ed
Anton Zaynutdinov <zaunytdinov@mail.ru>
2022-12-10 01:54:35 +0300

    initial commit

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,15 +1,12 @@
 package web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.dao.CarDAO;
-import web.dao.CarDAOImp;
+import web.config.service.CarService;
+import web.config.service.CarServiceImp;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,10 +24,9 @@ public class HelloController {
         return "index";
     }
 
-    CarDAO cars;
+    CarService cars;
 
-    @Autowired
-    public HelloController(CarDAO cars) {
+    public HelloController(CarServiceImp cars) {
         this.cars = cars;
     }
 

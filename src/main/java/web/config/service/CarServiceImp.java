07ee0d6284bffed7c534b96359ea2428ddc920b9
Anton Zaynutdinov <zaunytdinov@mail.ru>
2022-12-10 01:55:59 +0300

    initial commit

---
+++ src/main/java/web/config/service/CarServiceImp.java
@@ -0,0 +1,20 @@
+package web.config.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import web.dao.CarDAO;
+import web.model.Car;
+
+import java.util.List;
+
+@Component
+public class CarServiceImp implements CarService {
+
+    @Autowired
+    CarDAO carDAO;
+
+    @Override
+    public List<Car> printCars(int count) {
+        return carDAO.printCars(count);
+    }
+}
